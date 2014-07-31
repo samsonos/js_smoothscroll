66b65ea911b9a248b3cba7795246914010a7d1f0
Vitaly Iegorov <egorov@samsonos.com>
2014-07-31 19:33:36 +0300

    Added SamsonPHP connector class

---
+++ samson.php
@@ -0,0 +1,14 @@
+<?php
+namespace samson\js\smoothscroll;
+
+/**
+ * Интерфейс для подключения модуля в ядро фреймворка SamsonPHP
+ *
+ * @package SamsonPHP
+ * @author Vitaly Iegorov <vitalyiegorov@gmail.com> 
+ * @version 1
+ */
+class SJSSmoothScroll extends \samson\core\CompressableExternalModule
+{	
+	
+}
