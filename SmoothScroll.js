5b44abc380f139fd1b9d13f171b304fdc169e689
Vitaly Iegorov <egorov@samsonos.com>
2014-07-31 19:14:25 +0300

    Added composer.json

---
+++ SmoothScroll.js
@@ -0,0 +1,113 @@
+/**
+ * Created by Vitaly Egorov <egorov@samsonos.com> on 31.07.14.
+ */
+/**
+ * Generic javascript smooth scroll
+ */
+var sjsSmoothScroll = function(params) {
+    // Pointer to current object
+    var _self = this;
+
+    // Avoid errors when no params is passed
+    if(!params) params = {};
+
+    /** External before handler must return true to scroll */
+    var _beforeHandler = params.beforeHandler ? params.beforeHandler : function(){return true;};
+    /** External finish handler called when scroll is finished */
+    var _finishHandler = params.finishHandler ? params.finishHandler : function(){return true;};
+    /** Scroll speed */
+    var _speed = params.speed ? params.speed : 500;
+    /** Scroll speed when page is loaded if hash is present */
+    var _startSpeed = params.startSpeed ? params.startSpeed : 0;
+    /** Step to determine which anchor is active **/
+    var _menuStep = params.menuStep ? params._menuStep : 100;
+    /** CSS class to mark active link */
+    var _menuCSSActive = params.menuCSSActive ? params.menuCSSActive : 'active';
+
+    /**
+     * Get SamsonJS DOM node anchor from link
+     * @param link SamsonJS scroll link object
+     * @return SamsonJS|false DOM node anchor object
+     */
+    _self.getAnchor = function(link) {
+        // If SamsonJS object is passed - get href attribute other wise we think that this is hash string
+        var hash = typeof(link) == 'object' ? link.a('href').substring(link.a('href').indexOf('#')) : link;
+
+        // Get hash parameter from link URL
+        var href = hash.substring(hash.indexOf('#'));
+
+        // Try to find DOM object
+        var anchor = s(href);
+        if (anchor.length) {
+            // Return SamsonJS DOM node object
+            return anchor;
+        } else { // Return false to use in if's
+            return false;
+        }
+    };
+
+    // If we have hash pointer on plugin load
+    if (typeof window.location.hash != 'undefined') {
+        // Find anchor by URL hash identifier
+        var anchor = _self.getAnchor('#'+window.location.hash);
+        // Check if before handler returns true
+        if(anchor && _beforeHandler()) {
+            // Scroll window to this anchor
+            s.pageScrollTop(anchor.offset().top, _startSpeed, _finishHandler);
+        }
+    }
+
+    // Bind scroll event
+    s(window).scroll(function(){
+        // Get current scroll
+        var st = s.pageScrollTop();
+
+        // Current anchor index
+        var idx = 0;
+
+        // Iterate ALL scroll links to make the most relevant link active
+        _self.each(function(link){
+
+            // Find anchor by URL hash identifier
+            var anchor = _self.getAnchor(link);
+            if(anchor) {
+                // If scroll is near this anchor
+                if (anchor.offset().top - st < _menuStep) {
+
+                    // Make all links inactive
+                    _self.removeClass('active');
+
+                    // Activate current link matches this slide
+                    _self.elements[idx].addClass(_menuCSSActive);
+                }
+            }
+
+            // Increase anchor counter
+            idx++;
+        });
+    });
+
+    // Iterate all objects in current DOM collection
+    return _self.each(function(link){
+        // Find anchor by URL hash identifier
+        var anchor = _self.getAnchor(link);
+        // Check if before handler returns true
+        if(anchor) {
+            // Bind click event to scroll to anchor
+            link.click(function(){
+                // Call before handler
+                if (_beforeHandler()) {
+                    s.trace('scrolling page to '+anchor.a('id')+' '+anchor.offset().top);
+                    // Perform animation to scroll page to anchor
+                    s.scrollPageTo(anchor, _speed, _finishHandler);
+                }
+
+                // Ignore link default action propagation
+                return false;
+            });
+        }
+    });
+}
+
+/** Add smooth scroller to SamsonJS framework as plugin */
+SamsonJS.extend({ smoothScroll : sjsSmoothScroll });
